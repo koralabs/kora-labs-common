565aa5365b489de6b622efdae6bcf2056fced922
papag00se <jlanderson@gmail.com>
2024-05-01 12:48:36 -0700

    Modifying `IS_PRODUCTION` for frontend

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const IS_PRODUCTION = process.env.NODE_ENV?.trim() === 'production' && process.env.NETWORK?.toLowerCase() == 'mainnet';
 export const IS_SERVER = (typeof process !== 'undefined') && (typeof process.versions.node !== 'undefined');
+export const IS_PRODUCTION = IS_SERVER ? (process.env.NODE_ENV?.trim() === 'production' && process.env.NETWORK?.toLowerCase() == 'mainnet') : !(window.location.host.includes('preview.') || window.location.host.includes('preprod.')) ;
 export const AUTH_GRANT_DURATION = 1000 * 60 * 60 * 24 * 30; // 30 days
