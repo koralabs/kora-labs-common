909e8c8b7ecf69286a35ff8fdd4d76655f66fe1c
bigirishlion <bigirishlion@koralabs.io>
2024-05-02 11:58:46 -0700

    add oauthErrorCode to OauthAccessError

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,7 +1,17 @@
 export class OauthAccessError extends Error {
-    constructor(message: string)
-    {
+    oauthErrorCode: string;
+    constructor(
+        message: string,
+        oauthErrorCode:
+            | 'invalid_request'
+            | 'invalid_client'
+            | 'invalid_grant'
+            | 'unauthorized_client'
+            | 'unsupported_grant_type'
+            | 'invalid_scope'
+    ) {
         super();
         this.message = message;
+        this.oauthErrorCode = oauthErrorCode;
     }
 }
