2e7e0e9555c0681d61bd7905d6106a56e0f50ff2
bigirishlion <bigirishlion@koralabs.io>
2024-05-03 11:35:58 -0700

    have IS_PRODUCTION account for localhost

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,9 @@
-export const IS_SERVER = (typeof process !== 'undefined') && (typeof process.versions.node !== 'undefined');
-export const IS_PRODUCTION = IS_SERVER ? (process.env.NODE_ENV?.trim() === 'production' && process.env.NETWORK?.toLowerCase() == 'mainnet') : !(window.location.host.includes('preview.') || window.location.host.includes('preprod.')) ;
+export const IS_SERVER = typeof process !== 'undefined' && typeof process.versions.node !== 'undefined';
+export const IS_PRODUCTION = IS_SERVER
+    ? process.env.NODE_ENV?.trim() === 'production' && process.env.NETWORK?.toLowerCase() == 'mainnet'
+    : !(
+          window.location.host.includes('preview.') ||
+          window.location.host.includes('preprod.') ||
+          window.location.host.includes('localhost')
+      );
 export const AUTH_GRANT_DURATION = 1000 * 60 * 60 * 24 * 30; // 30 days
