1fab20f39676081979a9dcf2d7d5cf2eac800ef9
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-08-14 09:49:05 -0700

    chore: Update package.json dependencies and scripts for auth.handle.me db calls

--- src/repositories/interfaces.ts
+++ src/repositories/interfaces.ts
@@ -1,6 +1,10 @@
 export interface IOAuthRepo {
+    getClients(handle: string): Promise<AuthClient[]>;
+    saveClient(authClient: AuthClient): Promise<void>;
+    updateClient(clientId: string, updatedData: Partial<AuthClient>): Promise<void>;
+    deleteClient(clientId: string): Promise<void>;
     getClient(clientId?: string | null): Promise<AuthClient>;
-    getGrant(handle?:string | null, clientId?: string | null): Promise<AuthGrant>;
+    getGrant(handle?: string | null, clientId?: string | null): Promise<AuthGrant>;
     setGrant(grant?: AuthGrant): Promise<void>;
     getPermissions(): Promise<FriendlyPermissions>;
     validateAccessToken(clientId: string, handle: string, accessToken: string): Promise<boolean>;
@@ -10,11 +14,14 @@ export interface AuthClient {
     clientId: string;
     clientSecretHash: string;
     scope: Permission[];
+    projectDescription: string;
+    projectName: string;
     approvalUri: string;
     failureUri: string;
     handle: string;
-    applicationName: string;
     holderAddress: string;
+    applicationName: string;
+
 }
 
 export interface AuthGrant {
