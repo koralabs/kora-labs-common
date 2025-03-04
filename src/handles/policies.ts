1ac8382b3c36a15d7d4e3452aaf27ebbd5b07340
bigirishlion <bigirishlion@koralabs.io>
2025-03-04 15:23:19 -0800

    add handle policies interfaces

--- src/handles/policies.ts
+++ src/handles/policies.ts
@@ -1,7 +1,24 @@
 import { Network } from '../types';
 import { getSlotNumberFromDate } from '../utils';
 
-export const HANDLE_POLICIES = {
+interface PolicyIds {
+    [key: string]: {
+        firstMintingSlot: number;
+        lastMintingSlot: number | null;
+        sunsetSlot: number | null;
+        isDeMi?: boolean;
+    }
+}
+
+interface HandlePolicies {
+    'preview': PolicyIds;
+    'preprod': PolicyIds;
+    'mainnet': PolicyIds;
+    getActivePolicy(network: Network, atSlot?:number): string | undefined;
+    contains(network: Network, policyId: string): boolean;
+}
+
+export const HANDLE_POLICIES: HandlePolicies = {
     'preview': {
         'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null},
         '6c32db33a422e0bc2cb535bb850b5a6e9a9572222056d6ddc9cbc26e': { firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null, isDeMi: true }
