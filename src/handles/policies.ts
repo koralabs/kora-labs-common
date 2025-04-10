8bd4b54c59e29e4112809968b0425935324e4586
bigirishlion <bigirishlion@koralabs.io>
2025-04-10 16:34:39 -0700

    remove demi

--- src/handles/policies.ts
+++ src/handles/policies.ts
@@ -20,16 +20,13 @@ export interface HandlePolicies {
 
 export const HANDLE_POLICIES: HandlePolicies = {
     'preview': {
-        'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null},
-        '6c32db33a422e0bc2cb535bb850b5a6e9a9572222056d6ddc9cbc26e': { firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null, isDeMi: true }
+        'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null}
     },
     'preprod': {
-        'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null},
-        '6c32db33a422e0bc2cb535bb850b5a6e9a9572222056d6ddc9cbc26e': { firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null, isDeMi: true }
+        'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null}
     },
     'mainnet': {
-        'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null},
-        '6c32db33a422e0bc2cb535bb850b5a6e9a9572222056d6ddc9cbc26e': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null, isDeMi: true}
+        'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a': {firstMintingSlot: 0, lastMintingSlot: null, sunsetSlot: null}
     },
     
     getActivePolicy(network: Network, isDeMi = false, atSlot?:number) {
