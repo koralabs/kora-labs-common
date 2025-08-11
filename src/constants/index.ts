69c602620655bbb5b1d550da203690bfa685090f
papag00se <jlanderson@gmail.com>
2025-08-10 21:52:45 -0700

    Fixing bug with REDIS_HOST

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { mintedOgList } from './mintedOgList';
 export const IS_SERVER = typeof process !== 'undefined' && typeof process.versions.node !== 'undefined';
 export const IS_LOCAL = IS_SERVER ? process.env.IS_LOCAL === 'true' : window.location.host.includes('localhost');
 export const IS_AWS = IS_SERVER && !IS_LOCAL && process.env.AWS_REGION
-export const REDIS_HOST = process.env[`REDIS_HOST_${process.env.AWS_REGION}`.toUpperCase().replace('-', '_')] ?? process.env.REDIS_HOST ?? '127.0.0.1'
+export const REDIS_HOST = process.env[`REDIS_HOST_${process.env.AWS_REGION}`.toUpperCase().replace(/-/g, '_')] ?? process.env.REDIS_HOST ?? '127.0.0.1'
 export const NETWORK = process.env.NETWORK?.toLowerCase() ?? 'preview';
 export const IS_PRODUCTION = IS_SERVER
     ? process.env.NODE_ENV?.trim() === 'production' && NETWORK == 'mainnet'
